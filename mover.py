7fefc426dabc7802a95c506b1e40d5fade6a318a
Livid <livid@v2ex.com>
2020-09-09 00:09:29 -0700

    Only use builtin Python modules

--- mover.py
+++ mover.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 
-import arrow
+import datetime
 import getpass
-import gntp.notifier
 import os
 import os.path
 import shutil
@@ -11,10 +10,16 @@ import time
 
 
 def move():
+    today = datetime.datetime.today()
     prefix = '/Users/' + getpass.getuser() + '/'
     desktop = prefix + 'Desktop/'
     drawer = prefix + 'Documents/Drawer/'
-    dst = drawer + arrow.get().format('YYYYMM') + '/'
+    dst = drawer + today.strftime('%Y%m') + '/'
+
+    if not os.path.exists(dst):
+        os.mkdir(dst)
+        print("Folder created: %s" % (dst))
+
     files = os.listdir(desktop)
 
     now = time.time()
@@ -22,7 +27,7 @@ def move():
     days = 86400 * 7
 
     for item in files:
-        if not item.startswith('.'):
+        if (not item.startswith('.')) and (not item.endswith('.icloud')):
             file = desktop + item
             accessed = os.path.getatime(file)
             diff = now - accessed
@@ -30,7 +35,7 @@ def move():
                 print("Stale File: %s (Last Accessed: %f seconds ago)" % (file, diff))
                 shutil.move(file, dst + item)
                 message = item
-                gntp.notifier.mini(message, applicationName="Mover", title="File Moved")
+                #gntp.notifier.mini(message, applicationName="Mover", title="File Moved")
             else:
                 print("Fresh File: %s (Last Accessed: %f seconds ago)" % (file, diff))
 
